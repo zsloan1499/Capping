ed72ba0a5b81cfe94e74914e930017638f2b1908
Sloan7 <zachary.sloan1@marist.edu>
2024-09-15 14:16:16 -0400

    Update UserInfo.jsx

--- Project/my-app/app/components/UserInfo.jsx
+++ Project/my-app/app/components/UserInfo.jsx
@@ -13,10 +13,10 @@ export default function UserInfo() {
     return (
         <div>
             <div className="m-2">Homepage/UserInfo</div>
-            <div className="m-2">First Name: <span className="font-bold m-2">{session.user.fName || 'N/A'}</span></div>
-            <div className="m-2">Last Name: <span className="font-bold m-2">{session.user.lName || 'N/A'}</span></div>
-            <div className="m-2">UserName: <span className="font-bold m-2">{session.user.username || 'N/A'}</span></div>
-            <div className="m-2">Email: <span className="font-bold m-2">{session.user.email}</span></div>
+            <div className="m-2">First Name: <span className="font-bold m-2">{session?.user?.fName || 'N/A'}</span></div>
+            <div className="m-2">Last Name: <span className="font-bold m-2">{session?.user?.lName || 'N/A'}</span></div>
+            <div className="m-2">UserName: <span className="font-bold m-2">{session?.user?.username || 'N/A'}</span></div>
+            <div className="m-2">Email: <span className="font-bold m-2">{session?.user?.email}</span></div>
             <button onClick={() => signOut()} className="bg-red-600 m-2">Log Out</button>
             <br />
             <button className="bg-red-600 m-2">Delete Account</button>
