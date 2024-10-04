4cc7afcbf6f986aeb0caf917c19564fe93a4d78e
gioisles <giovanni.raia1@marist.edu>
2024-10-04 13:45:26 -0400

    HomePage UI

--- Project/my-app/app/components/HomePage.jsx
+++ Project/my-app/app/components/HomePage.jsx
@@ -16,10 +16,10 @@ export default function HomePage() {
   return (
     <div className="bg-customBlue w-screen h-screen flex">
       {/* Left Side Navigation Bar */}
-      <nav className={`bg-black ${isNavOpen ? 'w-32' : 'w-12'} h-full p-4 flex flex-col space-y-4 transition-width duration-300`}>
+      <nav className={`bg-black w-42 ${isNavOpen ? 'w-42' : 'w-42'} h-full p-4 flex flex-col space-y-4 transition-width duration-300`}>
         {/* Button to open/close the navigation */}
         <button 
-          className="bg-blue-500 text-white p-2 rounded mb-4" 
+          className="bg-blue-500 text-white p-2 rounded mb-4 w-16" 
           onClick={toggleNav}
         >
           {isNavOpen ? 'Close' : 'Open'}
